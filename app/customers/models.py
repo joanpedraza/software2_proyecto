380ec235964f6c39c81ea52cdf06fa43773ea178
DiegoGomez21 <dgomez@gmail.com>
2024-10-18 23:07:48 -0500

    Add new config

--- app/customers/models.py
+++ app/customers/models.py
@@ -2,9 +2,28 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
+from app.store_admin.models import Product, Supervisor
+
 User = get_user_model()
 
 # Create your models here.
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name=_("User"))
     
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    supervisor = models.ForeignKey(Supervisor, on_delete=models.CASCADE)
+    total_quantity = models.IntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+
+    def __str__(self):
+        return f'Order {self.id}'
+
+class ProductOrder(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.product.name} in Order {self.order.id}'
--- app/store_admin/models.py
+++ app/store_admin/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    provider = models.ForeignKey('Provider', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+class Provider(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.name
+
+class Supervisor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name=_("User"))
+    def __str__(self):
+        return self.user.first_name
