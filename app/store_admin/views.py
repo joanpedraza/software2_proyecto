2415314248b85198787d88dfd982707a7406d4e4
joanpedraza <sebaspedraza05@gmail.com>
2024-10-27 16:04:19 -0500

    Implementacion básica de las vista para añadir productos, ver el inventario y ver los pedidos

--- app/store_admin/views.py
+++ app/store_admin/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from app.store_admin.models import Product, Provider
+from app.customers.models import Order
 
 def admin_required(function=None, login_url='/accounts/login/'):
     return user_passes_test(lambda u: u.is_superuser, login_url=login_url)(function)
@@ -11,15 +13,32 @@ def dashboard(request):
 
 @admin_required
 def add_product(request):
-    return render(request, 'store_admin/addProducts.html')
+    if request.method == 'POST':
+        name = request.POST['name']
+        description = request.POST['description']
+        price = request.POST['price']
+        quantity = request.POST['quantity']
+        provider_id = request.POST['provider']
+
+        provider = Provider.objects.get(id=provider_id)
+
+        product = Product(name=name, description=description, price=price, quantity=quantity, provider=provider)
+        product.save()
+
+        return redirect('inventory')
+
+    providers = Provider.objects.all()
+    return render(request, 'store_admin/addProducts.html', {'providers': providers})
 
 @admin_required
 def inventory(request):
-    return render(request, 'store_admin/inventory.html')
+    products = Product.objects.all() 
+    return render(request, 'store_admin/inventory.html', {'products': products})
 
 @admin_required
 def order_history(request):
-    return render(request, 'store_admin/orders.html')
+    orders = Order.objects.all()  # Recupera todos los pedidos
+    return render(request, 'store_admin/orders.html', {'orders': orders})
 
 def exitAdmin(request):
     logout(request)
