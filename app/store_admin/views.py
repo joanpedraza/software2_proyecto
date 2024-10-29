9670d7ae974304366f4c34c3b99fa56c1e2804a2
joanpedraza <sebaspedraza05@gmail.com>
2024-10-28 23:59:50 -0500

    Filtro de pedidos

--- app/store_admin/views.py
+++ app/store_admin/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from app.store_admin.models import Product, Provider
-from app.customers.models import Order
+from app.customers.models import Order,Customer
 
 def admin_required(function=None, login_url='/accounts/login/'):
     return user_passes_test(lambda u: u.is_superuser, login_url=login_url)(function)
@@ -39,7 +39,24 @@ def inventory(request):
 @admin_required
 def order_history(request):
     orders = Order.objects.prefetch_related('productorder_set__product','customer','supervisor').all()
-    return render(request, 'store_admin/orders.html', {'orders': orders})
+    customers = Customer.objects.prefetch_related('user').all()
+
+    customer_id = request.GET.get('customer')
+    date = request.GET.get('date')
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+
+    if customer_id:
+        orders = orders.filter(customer_id=customer_id)
+    if date:
+        orders = orders.filter(date=date)
+    if min_price:
+        orders = orders.filter(total_price__gte=min_price)
+    if max_price:
+        orders = orders.filter(total_price__lte=max_price)
+        
+
+    return render(request, 'store_admin/orders.html', {'orders': orders, 'customers': customers})
 
 def exitAdmin(request):
     logout(request)
