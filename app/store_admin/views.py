bb2bc81cb691dd2a2de41f08cf0f21aee990a1a6
joanpedraza <sebaspedraza05@gmail.com>
2024-11-19 20:19:17 -0500

    Filtro de pedidos implementado

--- app/store_admin/views.py
+++ app/store_admin/views.py
@@ -41,22 +41,34 @@ def order_history(request):
     orders = Order.objects.prefetch_related('productorder_set__product','customer','supervisor').all()
     customers = Customer.objects.prefetch_related('user').all()
 
-    customer_id = request.GET.get('customer')
-    date = request.GET.get('date')
-    min_price = request.GET.get('min_price')
-    max_price = request.GET.get('max_price')
+    selected_customer = request.GET.get('customer')
+    selected_start_date = request.GET.get('start_date')
+    selected_end_date = request.GET.get('end_date')
+    selected_min_price = request.GET.get('min_price')
+    selected_max_price = request.GET.get('max_price')
 
-    if customer_id:
-        orders = orders.filter(customer_id=customer_id)
-    if date:
-        orders = orders.filter(date=date)
-    if min_price:
-        orders = orders.filter(total_price__gte=min_price)
-    if max_price:
-        orders = orders.filter(total_price__lte=max_price)
+    if selected_customer:
+        orders = orders.filter(customer_id=selected_customer)
+    
+    if selected_start_date:
+        orders = orders.filter(date__gte=selected_start_date)
+    
+    if selected_end_date:
+        orders = orders.filter(date__lte=selected_end_date)
+    
+    if selected_min_price:
+        orders = orders.filter(total_price__gte=selected_min_price)
+    
+    if selected_max_price:
+        orders = orders.filter(total_price__lte=selected_max_price)
         
 
-    return render(request, 'store_admin/orders.html', {'orders': orders, 'customers': customers})
+    return render(request, 'store_admin/orders.html', {'orders': orders, 'customers': customers,
+         'selected_customer': selected_customer,
+        'selected_start_date': selected_start_date,
+        'selected_end_date': selected_end_date,
+        'selected_min_price': selected_min_price,
+        'selected_max_price': selected_max_price})
 
 def exitAdmin(request):
     logout(request)
