eeb2b6a2c063b95fe18429a3aae3861cd4fd9a8a
DiegoGomez21 <dgomez@gmail.com>
2024-11-23 17:59:41 -0500

    Correccion

--- config/settings/base.py
+++ config/settings/base.py
@@ -65,9 +65,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-DATABASES = {
-    'default': env.db("DATABASE_URL")
-}
+DATABASES = env.db("DATABASE_URL", default="postgres:///invencloud")
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
