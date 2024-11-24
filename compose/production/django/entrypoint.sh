660849d0f1afe5701dcea161f8f7a98a1007058d
DiegoGomez21 <dgomez@gmail.com>
2024-11-23 19:50:42 -0500

    Edit entrypoint

--- compose/production/django/entrypoint.sh
+++ compose/production/django/entrypoint.sh
@@ -1,15 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-# Aplicar migraciones de la base de datos
+set -o errexit  # Salir si un comando falla
+set -o pipefail # Hacer que un pipe falle si un comando en el pipe falla
+set -o nounset  # Salir si se intenta usar una variable no definida
+
+# Limpiar archivos estáticos antiguos (opcional)
+# echo "Limpiando archivos estáticos antiguos..."
+# rm -rf staticfiles/*
+# rm -rf app/static/bundles/*
+
+# Preparar la base de datos y archivos estáticos
+echo "Aplicando migraciones de la base de datos..."
+python manage.py makemigrations
 python manage.py migrate
 
-# Instalar Gunicorn si no está instalado
-if ! pip show gunicorn > /dev/null 2>&1; then
-    echo "Instalando Gunicorn..."
-    pip install gunicorn
-fi
+# Recopilar archivos estáticos
+echo "Recopilando archivos estáticos..."
+python manage.py collectstatic --noinput
 
-PORT=${PORT:-8000} 
-
-# Ejecutar Gunicorn con el archivo WSGI de tu proyecto
-exec gunicorn config.wsgi:application --workers 3 --timeout 60 --bind 0.0.0.0:$PORT
+# Ejecutar Gunicorn
+PORT=${PORT:-8080}  # Cambiado a 8080 por defecto
+echo "Iniciando Gunicorn en el puerto $PORT..."
+exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --workers 3 --timeout 60
