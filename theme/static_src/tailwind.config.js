6f919a56ad3e067b8ea34f81a687efed4310264d
DiegoGomez21 <dgomez@gmail.com>
2024-11-23 21:51:12 -0500

    Tailwind config

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -1,20 +1,10 @@
+// tailwind.config.js
 module.exports = {
     important: false,
     content: [
-        // Templates dentro de la app theme (<tailwind_app_name>/templates), e.g. base.html
         '../templates/**/*.html',
-
-        // Directorio de templates principales del proyecto (BASE_DIR/templates)
         '../../templates/**/*.html',
-
-        // Templates en otras aplicaciones de Django (BASE_DIR/<app_name>/templates)
         '../../**/templates/**/*.html',
-
-        // JS: Si usas Tailwind CSS en JavaScript
-        // '../../**/*.js',
-
-        // Python: Si usas Tailwind CSS en Python
-        // '../../**/*.py'
     ],
     theme: {
         screens: {
@@ -30,14 +20,17 @@ module.exports = {
         require('@tailwindcss/typography'),
         require('@tailwindcss/aspect-ratio'),
     ],
-    // Asegúrate de purgar las clases no utilizadas en producción
     purge: {
         content: [
             './templates/**/*.html',
             './static/**/*.js',
         ],
         options: {
-            safelist: [], // Puedes agregar clases que quieras mantener sin purgar, si es necesario.
+            safelist: [],
         },
+    },
+    // Aquí especifica la ruta donde se deben almacenar los archivos de salida
+    output: {
+        css: './static/css/dist/styles.css', // Asegúrate de que Tailwind escriba aquí
     },
 };
