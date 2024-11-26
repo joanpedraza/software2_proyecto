fa382098e7c9a4e9b4d178e53c750a1eae6b39d0
DiegoGomez21 <dgomez@gmail.com>
2024-11-25 21:16:51 -0500

    Config staticfiles with WhiteNoise

--- app/store_admin/storage.py
+++ app/store_admin/storage.py
@@ -4,5 +4,5 @@ from django.conf import settings
 # Definir el almacenamiento para imágenes en el directorio staticfiles
 class StaticFilesStorage(FileSystemStorage):
     def __init__(self, *args, **kwargs):
-        kwargs['location'] = settings.STATIC_ROOT + '/product_images'
+        kwargs['location'] = str(settings.STATIC_ROOT) + '/product_images'  # Convertir a cadena
         super().__init__(*args, **kwargs)
